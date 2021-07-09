086f6b866253e278ab06cd38d67641b67aea902d
Jae-G <dbswo343434@gmail.com>
2021-07-09 14:14:31 +0900

    add reuse component product

---
+++ src/components/product.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="300px"
+    />
+
+    <v-card-title>
+      <b>{{ productNameChild }}</b>
+    </v-card-title>
+
+    <v-card-subtitle>
+      15,200
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props:{
+    productNameChild : String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
