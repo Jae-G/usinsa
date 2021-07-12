ba88d087efefac8faa57ff0ba29eb1bd7156a9f4
Jae-G <dbswo343434@gmail.com>
2021-07-12 22:10:09 +0900

    회원가입 유효성 검사와 파이어베이스 가입 구현

--- src/views/sign/SignUp.vue
+++ src/views/sign/SignUp.vue
@@ -12,7 +12,10 @@
         회원가입
       </h1>
     </v-card-title>
-    <v-form>
+    <v-form
+      ref="form"
+      v-model="isValid"
+    >
       <v-text-field
         v-model="email"
         outlined
@@ -40,6 +43,7 @@
       x-large
       dark
       block
+      @click="signUp"
     >
       회원가입
     </v-btn>
@@ -47,18 +51,25 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   data : () => ({
+    email : '',
     emailRules : [
       v => !!v || '이메일을 입력해주세요',
       v => /.+@.+\..+/.test(v) || '이메일 형식을 확인해주세요',
     ],
+
+    pwd : '',
     pwdRules : [
+      v => !!v || '비밀번호를 입력해주세요',
       v => v && v.length >= 8 || '최소 8자 이상 입력해주세요'
     ],
+    rePwd : '',
     rePwdRules : [
       v => !!v || '확인을 위해 비밀번호를 한 번 더 입력해주세요.'
     ],
+    isValid : false
 
   }),
 
@@ -66,6 +77,22 @@ export default {
     pwdConfirmRule() {
       return () => (this.pwd === this.rePwd) || '패스워드가 동일하지 않습니다.'
     },
+  },
+  methods : {
+    signUp() {
+      if (this.$refs.form.validate() === true) {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.email, this.pwd)
+          .then(() => {
+            alert('회원가입이 완료되어있습니다.')
+            this.$router.push('/')
+          })
+          .catch(err => {
+            alert(err.message)
+          })
+      }
+    }
   }
 }
 </script>
