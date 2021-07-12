1873985dcfcca4b89d2ca0138e34d10115ae3da5
Jae-G <dbswo343434@gmail.com>
2021-07-12 22:11:38 +0900

    메인화면 axios로 중앙 배너,상품데이터 가져올 수 있게 구현

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,9 +9,9 @@
       :cycle="true"
     >
       <v-carousel-item
-        v-for="(item,i) in items"
+        v-for="(midBanner,i) in midBannerInfo"
         :key="i"
-        :src="item.src"
+        :src="midBanner.imagePath"
       />
     </v-carousel>
 
@@ -21,14 +21,14 @@
           primary-title
           class="justify-center pb-12"
         >
-          <h2>신상품</h2>
+          <h2>제품</h2>
         </v-card-title>
         <v-row>
           <v-col
-            v-for="product in newProductsInfo"
+            v-for="product in productsInfo"
             :key="product.productCode"
             lg="3"
-            md="3"
+            md="4"
             sm="6"
             xs="12"
           >
@@ -53,51 +53,29 @@ export default {
     Product
   },
   data : () => ({
-    items: [
-      {
-        src: 'https://image.nbkorea.com/NBRB_Site/20210709/NB20210709101640222001.jpg',
-      },
-      {
-        src: 'https://image.nbkorea.com/NBRB_Site/20210708/NB20210708103655270001.jpg',
-      },
-      {
-        src: 'https://image.nbkorea.com/NBRB_Site/20210621/NB20210621101537543001.jpg',
-      },
-      {
-        src: 'https://image.nbkorea.com/NBRB_Site/20210625/NB20210625134501151001.jpg',
-      },
-    ],
-    newProductsInfo: [
-      {
-        productCode : '1',
-        productName : '빨간 티셔츠',
-        productPrice : '20000',
-        productImagePath : 'https://cdn.pixabay.com/photo/2016/12/06/09/31/blank-1886008_960_720.png',
-        productLike : 2
-      },
-      {
-        productCode : '2',
-        productName : '나이키 조던 84',
-        productPrice : '125000',
-        productImagePath : 'https://cdn.pixabay.com/photo/2019/01/26/22/48/nike-3957127_960_720.jpg',
-        productLike : 3687
-      },
-      {
-        productCode : '3',
-        productName : '양말 (3pack)',
-        productPrice : '25000',
-        productImagePath : 'https://cdn.pixabay.com/photo/2019/07/11/08/19/sock-4330279_960_720.jpg',
-        productLike : 42387
-      },
-      {
-        productCode : '4',
-        productName : '컨버스',
-        productPrice : '75000',
-        productImagePath : 'https://cdn.pixabay.com/photo/2017/06/09/18/28/sneakers-2387899_960_720.jpg',
-        productLike : 2
-      },
-    ]
-  })
+    midBannerInfo: [],
+    productsInfo: [],
+  }),
+  created(){
+    this.getMainData()
+  },
+  methods : {
+    async getMainData() {
+      try {
+        let response
+        //상품 데이터 가져오기
+        response = await this.$axios.get(process.env.VUE_APP_FIREBASE_URL + 'product.json')
+        this.productsInfo = response.data.filter((product) => product.useYn === 'y')
+        //console.log(this.productInfo)
+        //중앙 배너 이미지 가져오기
+        response = await this.$axios.get(process.env.VUE_APP_FIREBASE_URL + 'mid-banner.json')
+        this.midBannerInfo = response.data.filter((product) => product.useYn === 'y')
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+
 }
 </script>
 
