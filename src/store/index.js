b9c0c170040c1f6d4f07bfb941ac6ed5c7c00706
Jae-G <dbswo343434@gmail.com>
2021-07-12 22:12:34 +0900

    state에 이미지, 토큰 넣는 로직 구현

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    email : '',
+    token : '',
   },
   mutations: {
+    SET_CURRENT_EMAIL(state,email) {
+      state.email = email
+    },
+    SET_CURRENT_TOKEN(state,token) {
+      state.token = token
+    }
   },
   actions: {
+    async setUserInfo(context,email,token) {
+      console.log(context)
+      context.commit('SET_CURRENT_EMAIL',email)
+      context.commit('SET_CURRENT_TOKEN',token)
+    }
   },
   modules: {
   }
