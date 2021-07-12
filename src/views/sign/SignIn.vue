44a6e318a016beb6a282adca174cddc219020c04
Jae-G <dbswo343434@gmail.com>
2021-07-12 22:10:24 +0900

    로그인 유효성 검사와  파이어베이스 인증 구현

--- src/views/sign/SignIn.vue
+++ src/views/sign/SignIn.vue
@@ -12,27 +12,68 @@
         로그인
       </h1>
     </v-card-title>
-    <v-text-field
-      outlined
-      label="이메일"
-    />
-    <v-text-field
-      outlined
-      label="패스워드"
-    />
-    <v-btn
-      x-large
-      dark
-      block
+    <v-form
+      ref="form"
     >
-      로그인
-    </v-btn>
+      <v-text-field
+        v-model="email"
+        outlined
+        label="이메일"
+      />
+      <v-text-field
+        v-model="pwd"
+        outlined
+        label="패스워드"
+        type="password"
+      />
+      <v-btn
+        x-large
+        dark
+        block
+        @click="signIn"
+      >
+        로그인
+      </v-btn>
+    </v-form>
   </v-card>
 </template>
 
 <script>
-export default {
+import firebase from "firebase";
 
+export default {
+  data: () => ({
+    email : '',
+    emailRules : [
+      v => !!v || '이메일을 입력해주세요',
+      v => /.+@.+\..+/.test(v) || '이메일 형식을 확인해주세요',
+    ],
+    pwd : '',
+    pwdRules : [
+      v => !!v || '비밀번호를 입력해주세요',
+    ]
+  }),
+  methods: {
+    signIn() {
+      if (this.$refs.form.validate() === true) {
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(this.email,this.pwd )
+          .then((res) => {
+            this.$store.dispatch('setUserInfo',res.user.email,res.user.l)
+            this.$router.push('/')
+          })
+          .catch((err) => {
+            console.log(err)
+            alert('이메일 또는 비밀번호를 확인해주세요')
+          })
+      }
+      else {
+        alert('이메일 또는 비밀번호를 확인해주세요')
+      }
+    }
+
+  }
 }
 </script>
 
