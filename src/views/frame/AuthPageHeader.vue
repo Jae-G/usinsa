bd3a3ca605c10fc16cd83986f1bdc923d189b051
Jae-G <dbswo343434@gmail.com>
2021-07-15 19:44:36 +0900

    AuthPageHeader 추가

---
+++ src/views/frame/AuthPageHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app-bar
+    elevate-on-scroll
+    tile
+    color="white"
+    flat
+    app
+  >
+    <v-btn
+      id="no-background-hover"
+      href="/"
+      color="transparent"
+      class="elevation-0 black--text"
+      :ripple="false"
+      x-large
+      @click="moveToPage_mixin('/')"
+    >
+      <h2>
+        USINSA
+      </h2>
+    </v-btn>
+
+    <v-spacer />
+
+    <v-toolbar-items>
+      <v-btn
+        v-ripple="false"
+        color="transparent"
+        class="elevation-0 black--text"
+        @click="moveToPage_mixin('/auth/sign-in')"
+      >
+        <h3>로그인</h3>
+      </v-btn>
+      <v-btn
+        color="transparent"
+        class="elevation-0 black--text"
+        @click="moveToPage_mixin('/auth/sign-up')"
+      >
+        <h3>회원가입</h3>
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+import MainMixin from '../../mixin/index'
+export default {
+  mixins:[MainMixin],
+  data: () => ({
+
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
