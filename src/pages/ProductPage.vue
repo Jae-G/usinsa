9d78fb137b5d8e60bb2d0c5e4e3352e43f382b27
Jae-G <dbswo343434@gmail.com>
2021-07-15 19:43:29 +0900

    ProductPage 추가

---
+++ src/pages/ProductPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from '@/views/frame/MainPageHeader.vue'
+export default {
+  components : {
+    Header
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
