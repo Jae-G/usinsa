6233d487d9bbd4d6692eef1ba899d443542a8305
Jae-G <dbswo343434@gmail.com>
2021-07-19 20:27:34 +0900

    firebaseconfig 추가 및 axios 전역설정

--- src/main.js
+++ src/main.js
@@ -3,11 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-// import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
-// import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
 import VueFullPage from 'vue-fullpage.js'
+import axios from 'axios'
+import firebase from 'firebase'
 
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_URL,
+  authDomain: "usinsa-4be58.firebaseapp.com",
+  databaseURL: "https://usinsa-4be58-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "usinsa-4be58",
+  storageBucket: "usinsa-4be58.appspot.com",
+  messagingSenderId: "48688437796",
+  appId: "1:48688437796:web:bbec7b8292260f91ab16b1",
+  measurementId: "G-0X9NBBD58V"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+
+Vue.prototype.$axios = axios
 Vue.use(VueFullPage)
+
+
 
 Vue.config.productionTip = false
 new Vue({
