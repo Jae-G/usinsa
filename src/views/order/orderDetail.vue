e36721c48ce9cf98020b962dda599fa13bbe589c
Jae-G <dbswo343434@gmail.com>
2021-07-20 21:59:55 +0900

    orderDetail 추가

---
+++ src/views/order/orderDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <dl>
+      <dt>판매가</dt>
+      <dd> {{ productInfo.productPrice.length !== 0 && productInfo.productPrice | comma }}원</dd>
+    </dl>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
